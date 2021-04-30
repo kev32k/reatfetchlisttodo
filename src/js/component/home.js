a428f23db01deb6dc286d6969abe80b70e1cd16e
kev32k <lealjosue@icloud.com>
2021-04-30 22:35:14 +0000

    fetchlistreacttodo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [list, setlist] = useState([]);
+	let url = "https://assets.breatheco.de/apis/fake/todos/user/kev32k";
+
+	function handleEvent(e) {
+		if (e.key === "Enter") {
+			setlist([...list, { label: e.target.value, done: false }]);
+		}
+	}
+
+	const removeItem = indexItem => {
+		setlist(prevState =>
+			prevState.filter((todo, index) => index !== indexItem)
+		);
+	};
+
+	const updateList = theList => {
+		fetch(url, {
+			headers: { "Content-Type": "application/json" },
+			method: "PUT",
+			body: JSON.stringify(theList)
+		})
+			.then(response => response.json())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	};
+
+	async function deleteALL() {
+		//delete
+		await fetch(url, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+
+		//POST
+
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify([]);
+
+		var requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw
+		};
+
+		await fetch(url, requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+
+		//GET
+		fetch(url)
+			.then(response => response.json())
+			.then(result => setlist(result))
+			.catch(error => console.log("error", error));
+	}
+
+	useEffect(() => {
+		fetch(url)
+			.then(response => response.json())
+			.then(result => setlist(result))
+			.catch(error => console.log("error", error));
+	}, []);
+
+	useEffect(() => {
+		updateList(list);
+	}, [list]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="box">
+				<h1>Todo List</h1>
+
+				<input
+					type="text"
+					placeholder="Fill"
+					onKeyDown={event => handleEvent(event)}
+					className="m-2"
+				/>
+				<button
+					className="button2 btn  m-3"
+					onClick={() => deleteALL()}>
+					Delete List
+				</button>
+				<div className="list-container">
+					<ul>
+						{list.map((item, index) => {
+							return (
+								<div className="list-box" key={index}>
+									<li>{item.label}</li>
+									<button
+										className="button1 btn"
+										onClick={() => removeItem(index)}>
+										X
+									</button>
+								</div>
+							);
+						})}
+						<div id="espacio"></div>
+						<p>{list.length + "   item added"}</p>
+						<div id="espacio"></div>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
